cc01eba22951322dbc670b650b4ca21797ec353a
leavescy@126.com <leavescy@126.com>
2021-10-27 15:25:32 +0800

    完善agg的unitest

--- unitest/aggregation_sql_test.cpp
+++ unitest/aggregation_sql_test.cpp
@@ -8,11 +8,11 @@
 class AggregationSQLTest : public SQLTest {
   // 在每个 TEST_F 之前执行的内容
   void BeforeCase() override {
-    ExecuteSql("create table t(id int);");
-    ExecuteSql("insert into t values(1);");
-    ExecuteSql("insert into t values(2);");
-    ExecuteSql("insert into t values(3);");
-    ExecuteSql("insert into t values(4);");
+    ExecuteSql("create table t(id int, name char, price float);");
+    ExecuteSql("insert into t values (1,'ali',1.111);");
+    ExecuteSql("insert into t values (2,'bob',2.222);");
+    ExecuteSql("insert into t values (3,'cin',3.333);");
+    ExecuteSql("insert into t values (4,'def',4.444);");
   }
 
   // 在每个 TEST_F 之后执行的内容
@@ -21,10 +21,18 @@ class AggregationSQLTest : public SQLTest {
 
 // 不对 TEST_F 的执行顺序提供保证
 TEST_F(AggregationSQLTest, aggregation) {
+  ASSERT_EQ(ExecuteSql("select * from t;"), "id | name | price\n1 | ali | 1.11\n2 | bob | 2.22\n3 | cin | 3.33\n4 | def | 4.44\n");
   ASSERT_EQ(ExecuteSql("select count(*) from t;"), "count(*)\n4\n");
   ASSERT_EQ(ExecuteSql("select count(1) from t;"), "count(1)\n4\n");
   ASSERT_EQ(ExecuteSql("select count(id) from t;"), "count(id)\n4\n");
   ASSERT_EQ(ExecuteSql("select min(id) from t;"), "min(id)\n1\n");
+  ASSERT_EQ(ExecuteSql("select min(name) from t;"), "min(name)\nali\n");
   ASSERT_EQ(ExecuteSql("select max(id) from t;"), "max(id)\n4\n");
+  ASSERT_EQ(ExecuteSql("select max(name) from t;"), "max(name)\ndef\n");
   ASSERT_EQ(ExecuteSql("select avg(id) from t;"), "avg(id)\n2.5\n");
+  ASSERT_EQ(ExecuteSql("select avg(price) from t;"), "avg(price)\n2.78\n");
+  ASSERT_EQ(ExecuteSql("select avg(price), max(id), min(name) from t;"), "avg(price) | max(id) | min(name)\n2.78 | 4 | ali\n");
+  ASSERT_EQ(ExecuteSql("select count(*,id) from t;"), "FAILURE\n");
+  ASSERT_EQ(ExecuteSql("select count() from t;"), "FAILURE\n");
+  ASSERT_EQ(ExecuteSql("select count(not_exists_col) from t;"), "FAILURE\n");
 }
