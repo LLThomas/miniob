4242d287c2113bbfe26ef27cb90f8e9b61506ca7
Zhenhua Yang <huaouo@gmail.com>
2021-10-28 11:27:15 +0800

    hotfix: MultiInsertTest 增加 Date 类型

--- unitest/multi_insert_test.cpp
+++ unitest/multi_insert_test.cpp
@@ -5,19 +5,20 @@
 #include <include/sql_test.h>
 
 class MultiInsertTest : public SQLTest {
-  void BeforeCase() override { ExecuteSql("create table t (a int, b int);"); }
+  void BeforeCase() override { ExecuteSql("create table t (a int, b date);"); }
 
   void AfterCase() override { ExecuteSql("drop table t;"); }
 };
 
 TEST_F(MultiInsertTest, insert_success) {
-  ASSERT_EQ(ExecuteSql("insert into t values (1,1),(2,2);"), "SUCCESS\n");
+  ASSERT_EQ(ExecuteSql("insert into t values (1,'2020-1-1'),(2,'2020-2-3');"), "SUCCESS\n");
   ASSERT_EQ(ExecuteSql("select a from t;"), "a\n1\n2\n");
 }
 
 TEST_F(MultiInsertTest, insert_failure) {
-  ASSERT_EQ(ExecuteSql("insert into t values (1,1),('f',2);"), "FAILURE\n");
-  ASSERT_EQ(ExecuteSql("insert into t values (1,1),(2);"), "FAILURE\n");
-  ASSERT_EQ(ExecuteSql("insert into t values (1,1),();"), "FAILURE\n");
+  ASSERT_EQ(ExecuteSql("insert into t values (1,'2020-1-1'),(2,'2020-0-3');"), "FAILURE\n");
+  ASSERT_EQ(ExecuteSql("insert into t values (1,'2020-1-1'),(2,243);"), "FAILURE\n");
+  ASSERT_EQ(ExecuteSql("insert into t values (1,'2020-1-1'),(2,'f');"), "FAILURE\n");
+  ASSERT_EQ(ExecuteSql("insert into t values (1,'2020-1-1'),();"), "FAILURE\n");
   ASSERT_EQ(ExecuteSql("select a from t;"), "a\n");
 }
